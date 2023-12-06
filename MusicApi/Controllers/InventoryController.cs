7d327bae31576f715e884253e59310dfde1a34f9
parker.swenson1 <parkerswenson00@gmail.com>
2023-12-05 17:58:46 -0700

    Inventory Controller working time to put it in the code

---
+++ MusicApi/Controllers/InventoryController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicApi.Data;
+using MusicApi.Request;
+using MusicApi.Services;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+
+[Route("[controller]")]
+[ApiController]
+public class InventoryController : Controller
+{
+    private readonly ILogger<InventoryController> logger;
+    private readonly IInventoryService<Inventory> inventoryService;
+
+    public InventoryController(ILogger<InventoryController> logger, IInventoryService<Inventory> inventoryService)
+    {
+        this.logger = logger;
+        this.inventoryService = inventoryService;
+    }
+
+    [HttpPatch("{inventoryRequest}")]
+    public async Task Update(AddInventoryRequest request)
+    {
+        var Inventory = new Inventory()
+        {
+            Id = request.Id,
+            ItemId = request.ItemId,
+            StatusId = request.StatusId,
+            IsRentable = request.IsRentable,
+            IsPurchased = request.IsPurchased,
+        };
+
+        await inventoryService.Update(Inventory);
+    }
+}
---
+++ MusicApi/Services/InventoryService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using MusicApi.Data;
+
+namespace MusicApi.Services;
+
+public class InventoryService : IInventoryService<Inventory>
+{
+    private readonly ILogger<InventoryService> logger;
+    private IDbContextFactory<MusicDbContext> contextFactory;
+
+    public InventoryService(ILogger<InventoryService> logger, IDbContextFactory<MusicDbContext> contextFactory)
+    {
+        this.logger = logger;
+        this.contextFactory = contextFactory;
+    } 
+
+    public async Task<IEnumerable<Inventory>> GetAll()
+    {
+        var context = contextFactory.CreateDbContext(); 
+        return await context.Inventories.ToListAsync();
+    }
+
+    public async Task<Inventory> Update(Inventory inventory)
+    {
+        var context = contextFactory.CreateDbContext();
+        var iuc = await context.Inventories.Where(i => i.ItemId == inventory.ItemId).FirstOrDefaultAsync();
+
+        iuc.ItemId = inventory.ItemId;
+        iuc.StatusId = inventory.StatusId;
+        iuc.IsRentable = inventory.IsRentable;
+        iuc.IsPurchased = inventory.IsPurchased;
+
+        context.Inventories.Update(iuc);
+        await context.SaveChangesAsync();
+
+        return iuc;
+    }
+}
